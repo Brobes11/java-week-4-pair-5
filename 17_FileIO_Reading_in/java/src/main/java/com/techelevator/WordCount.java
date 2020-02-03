f15a7c9790457bfea88e745e421208f2cb2cc843
TaylorB <taylor.broberg@gmail.com>
2020-02-03 15:40:08 -0500

    pairs exercise complete

--- 17_FileIO_Reading_in/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/java/src/main/java/com/techelevator/WordCount.java
@@ -2,13 +2,40 @@ package com.techelevator;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class WordCount {
 
 	public static void main(String[] args) {
+		File myFile = new File("alice.txt");
+		int wordCount = 0;
+		try (Scanner myScanner = new Scanner(myFile)) {
+			while (myScanner.hasNextLine()) {
+				String currentLine = myScanner.nextLine().trim();
+				if (currentLine.isEmpty() == false) {
+					String[] words = currentLine.split(" +");
+					wordCount += words.length;
+				}
+			}
+			System.out.println("Word Count: " + wordCount);
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found...");
+		}
+		int sentanceCount = 0;
+
+		try (Scanner myScanner = new Scanner(myFile)) {
+			while (myScanner.hasNextLine()) {
+				String currentLine = myScanner.nextLine();
+				if (currentLine.contains(".")) {
+					sentanceCount++;
+				}
+			}
+			System.out.print("Sentance Count: " + sentanceCount);
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found...");
+		}
 
 	}
-
 }
